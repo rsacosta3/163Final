90ffc74239c6af0c072db5bed969888332ec45c2
NicG24 <nickgomez24@outlook.com>
2025-06-01 18:28:32 -0700

    had the nationals mislabeled

--- team.js
+++ team.js
@@ -276,14 +276,14 @@ function createSpendingWinsComparisonChart(teamName, teamAbbr) {
         'AL East': ['NYY', 'BOS', 'TBR', 'TOR', 'BAL'],
         'AL Central': ['CHW', 'CLE', 'DET', 'KCR', 'MIN'],
         'AL West': ['HOU', 'LAA', 'OAK', 'SEA', 'TEX'],
-        'NL East': ['ATL', 'MIA', 'NYM', 'PHI', 'WSN'],
+        'NL East': ['ATL', 'MIA', 'NYM', 'PHI', 'WSH'],
         'NL Central': ['CHC', 'CIN', 'MIL', 'PIT', 'STL'],
         'NL West': ['ARI', 'COL', 'LAD', 'SDP', 'SFG']
     };
     // Leagues
     const leagues = {
         'AL': ['NYY', 'BOS', 'TBR', 'TOR', 'BAL', 'CHW', 'CLE', 'DET', 'KCR', 'MIN', 'HOU', 'LAA', 'OAK', 'SEA', 'TEX'],
-        'NL': ['ATL', 'MIA', 'NYM', 'PHI', 'WSN', 'CHC', 'CIN', 'MIL', 'PIT', 'STL', 'ARI', 'COL', 'LAD', 'SDP', 'SFG']
+        'NL': ['ATL', 'MIA', 'NYM', 'PHI', 'WSH', 'CHC', 'CIN', 'MIL', 'PIT', 'STL', 'ARI', 'COL', 'LAD', 'SDP', 'SFG']
     };
 
     // Find teams division and leaue
